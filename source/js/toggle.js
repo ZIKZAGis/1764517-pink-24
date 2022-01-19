59b0c6d5e8d27e5a7a5315b18222227b8d44feda
Evgeniy <nikolaev_zheka-zik@bk.ru>
2022-01-19 15:45:24 +0300

    работает без js

--- source/js/toggle.js
+++ source/js/toggle.js
@@ -1,6 +1,8 @@
 let navMain = document.querySelector('.page-header__navigation');
 let navToggle = document.querySelector('.main-nav__toggle');
 
+navMain.classList.remove('page-header__navigation--nojs');
+
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('page-header__navigation--closed')) {
     navMain.classList.remove('page-header__navigation--closed');
